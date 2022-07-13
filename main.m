75dc02975ac422f6caa20952877b62462d6ada9e
leejiwon1202 <86599323+leejiwon1202@users.noreply.github.com>
2022-07-13 19:42:12 +0900

    Update main.m

--- main.m
+++ main.m
@@ -19,40 +19,42 @@ takeoff(droneObj);
 moveup(droneObj,'Distance', 0.3,'WaitUntilDone', true);
 moveforward(droneObj, 'Distance', 1.5, 'WaitUntilDone', true);
 
-findRedDot = false;
+findGreenDot = false;
 moveforward(droneObj, 'Distance', dist_forward, 'WaitUntilDone', true);
-while(~findRedDot)
+while(~findGreenDot)
     frame = snapshot(cam);
-    r = frame(:,:,1);   detect_r = (r > 90) & (r < 200);
-    g = frame(:,:,2);   detect_g = (g < 55);
-    b = frame(:,:,3);   detect_b = (b < 80);
-    detect_Rdot = detect_r & detect_g & detect_b;
-    red_sum = sum(sum(detect_Rdot));
+    frame = rgb2hsv(frame);
+    h = frame(:,:,1); detect_h = (h >= 0.2) & (h <= 0.42);
+    s = frame(:,:,2); detect_s = (s >= 0.1) & (s <= 0.76);
+    detect_Gdot = detect_h & detect_s;
+    canny_img = edge(detect_Gdot, 'Canny', 0.9, 8);
+    fill_img = imfill(canny_img, 'holes');
+    green_sum = sum(sum(fill_img));
 
     detecting = false;
-    if red_sum == 0
+    if green_sum == 0
         moveback(droneObj,'Distance', dist_backward,'WaitUntilDone', true);
         continue;
-    elseif red_sum >= 3000
+    elseif green_sum >= 3000
         turn(droneObj, deg2rad(90));
         moveforward(droneObj, 'Distance', dist_pass, 'WaitUntilDone', true);
-        findRedDot = true;
-    elseif red_sum >= 2000
+        findGreenDot = true;
+    elseif green_sum >= 2000
         r_center = sum(sum(detect_Rdot(fix(end/3):fix(end/3 * 2), fix(end/3):fix(end/3 * 2))));
         if r_center < 2000
             detecting = true;
         end
-    elseif red_sum >= 300
+    elseif green_sum >= 300
         detecting = true;
     end
 
     if (detecting)
-        r_lst = [sum(sum(detect_Rdot(1:fix(end/4*3), 1:end)))
-                 sum(sum(detect_Rdot(fix(end/4):end, 1:end)))
-                 sum(sum(detect_Rdot(1:end, 1:fix(end/3*2))))
-                 sum(sum(detect_Rdot(1:end, fix(end/3):end)))];
-        if abs(r_lst(1) - r_lst(2)) > 100
-            if r_lst(1) >= r_lst(2) 
+        g_lst = [sum(sum(fill_img(1:fix(end/4*3), 1:end)))
+                 sum(sum(fill_img(fix(end/4):end, 1:end)))
+                 sum(sum(fill_img(1:end, 1:fix(end/3*2))))
+                 sum(sum(fill_img(1:end, fix(end/3):end)))];
+        if abs(g_lst(1) - g_lst(2)) > 100
+            if g_lst(1) >= g_lst(2) 
                 moveup(droneObj,'Distance', Ddist_udlr,'WaitUntilDone', true);
                 height = height + Ddist_udlr;
             else
@@ -60,8 +62,8 @@ while(~findRedDot)
                 height = height - Ddist_udlr;
             end
         end
-        if abs(r_lst(3) - r_lst(4)) > 100
-            if r_lst(3) >= r_lst(4)
+        if abs(g_lst(3) - g_lst(4)) > 100
+            if g_lst(3) >= g_lst(4)
                 moveleft(droneObj,'Distance', Ddist_udlr,'WaitUntilDone', true);
             else
                 moveright(droneObj,'Distance', Ddist_udlr,'WaitUntilDone', true);
@@ -69,7 +71,7 @@ while(~findRedDot)
         end
     end
     
-    if (~findRedDot)
+    if (~findGreenDot)
         moveforward(droneObj, 'Distance', dist_forward, 'WaitUntilDone', true);
     end
 end
@@ -161,37 +163,37 @@ while(~findRightPos)
     end
 end
 
-findGreenDot = false;
+findPurpleDot = false;
 moveforward(droneObj, 'Distance', dist_forward, 'WaitUntilDone', true);
-while(~findGreenDot)
+while(~findPurpleDot)
     frame = snapshot(cam);
     frame = rgb2hsv(frame);
-    h = frame(:,:,1); detect_h = (h >= 0.2) & (h <=0.42);
-    s = frame(:,:,2); detect_s = (s >= 0.1) & (s <= 0.65);
-    detect_Gdot = detect_h & detect_s;
-    canny_img = edge(detect_Gdot, 'Canny', 0.9, 8);
+    h = frame(:,:,1); detect_h = (h >= 0.65) & (h <= 0.8);
+    s = frame(:,:,2); detect_s = (s >= 0.3) & (s <= 0.85);
+    detect_Pdot = detect_h & detect_s;
+    canny_img = edge(detect_Pdot, 'Canny', 0.9, 9);
     fill_img = imfill(canny_img, 'holes');
-    green_sum = sum(sum(fill_img));
+    purple_sum = sum(sum(fill_img));
     
     detecting = false;
-    if green_sum == 0
+    if purple_sum == 0
         moveback(droneObj,'Distance', dist_backward,'WaitUntilDone', true);
         continue;
-    elseif green_sum >= 2000
+    elseif purple_sum >= 2000
         turn(droneObj, deg2rad(90));
         moveforward(droneObj, 'Distance', dist_pass, 'WaitUntilDone', true);
         findGreenDot = true;
-    elseif green_sum >= 300
+    elseif purple_sum >= 300
         detecting = true;
     end
 
     if (detecting)
-        g_lst = [sum(sum(fill_img(1:fix(end/4*3), 1:end)))
+        p_lst = [sum(sum(fill_img(1:fix(end/4*3), 1:end)))
                  sum(sum(fill_img(fix(end/4):end, 1:end)))
                  sum(sum(fill_img(1:end, 1:fix(end/3*2))))
                  sum(sum(fill_img(1:end, fix(end/3):end)))];
-        if abs(g_lst(1) - g_lst(2)) > 100
-            if g_lst(1) >= g_lst(2)
+        if abs(p_lst(1) - p_lst(2)) > 100
+            if p_lst(1) >= p_lst(2)
                 moveup(droneObj,'Distance', Ddist_udlr,'WaitUntilDone', true);
                 height = height + Ddist_udlr;
             else
@@ -199,8 +201,8 @@ while(~findGreenDot)
                 height = height - Ddist_udlr;
             end
         end
-        if abs(g_lst(3) - g_lst(4)) > 100
-            if g_lst(3) >= g_lst(4)
+        if abs(p_lst(3) - p_lst(4)) > 100
+            if p_lst(3) >= p_lst(4)
                 moveleft(droneObj,'Distance', Ddist_udlr,'WaitUntilDone', true);
             else
                 moveright(droneObj,'Distance', Ddist_udlr,'WaitUntilDone', true);
@@ -208,7 +210,7 @@ while(~findGreenDot)
         end
     end
     
-    if (~findGreenDot)
+    if (~findPurpleDot)
         moveforward(droneObj, 'Distance', dist_forward, 'WaitUntilDone', true);
     end
 end
@@ -322,26 +324,24 @@ while(~findRightPos)
     end
 end
 
-findPurpleDot = false;
+findRedDot = false;
 moveforward(droneObj, 'Distance', dist_forward, 'WaitUntilDone', true);
-while(~findPurpleDot)
+while(~findRedDot)
     frame = snapshot(cam);
-    frame = rgb2hsv(frame);
-    h = frame(:,:,1); detect_h = (h >= 0.65) & (h <= 0.8);
-    s = frame(:,:,2); detect_s = (s >= 0.3) & (s <= 0.85);
-    detect_Pdot = detect_h & detect_s;
-    canny_img = edge(detect_Pdot, 'Canny', 0.9, 9);
-    fill_img = imfill(canny_img, 'holes');
-    purple_sum = sum(sum(fill_img));
+    r = frame(:,:,1);   detect_r = (r > 90) & (r < 200);
+    g = frame(:,:,2);   detect_g = (g < 55);
+    b = frame(:,:,3);   detect_b = (b < 80);
+    detect_Rdot = detect_r & detect_g & detect_b;
+    red_sum = sum(sum(detect_Rdot));
 
     detecting = false;
-    if purple_sum == 0
+    if red_sum == 0
         moveback(droneObj,'Distance', dist_backward,'WaitUntilDone', true);
         continue;
-    elseif purple_sum >= 2000
+    elseif red_sum >= 2000
         land(droneObj);
-        findPurpleDot = true;
-    elseif purple_sum >= 300
+        findRedDot = true;
+    elseif red_sum >= 300
         detecting = true;
     end
 
